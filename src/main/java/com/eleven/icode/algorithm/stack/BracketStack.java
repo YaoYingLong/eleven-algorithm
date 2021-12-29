a601616a4317d191c1ac569cc9af37a26233bea3
yao_yinglong <yao_yinglong@icekredit.com>
2021-12-29 18:20:17 +0800

    feat: 补充排序算法2

--- src/main/java/com/eleven/icode/algorithm/stack/BracketStack.java
+++ src/main/java/com/eleven/icode/algorithm/stack/BracketStack.java
@@ -3,10 +3,16 @@ package com.eleven.icode.algorithm.stack;
 import java.util.Scanner;
 
 /**
+ * 四则运算就是通过栈来实现的
+ * 遇到是数字 就直接入栈到数字栈里面去。
+ * 遇到是符号 就把符号栈的栈顶拿出来做比较。若他比栈顶符号的优先级高就直接入栈，
+ * 若比符号栈顶的优先级低或者相同，就从符号栈里面取栈顶进行计算（从数字栈中取栈顶的2个数）
+ * 计算完的结果还要再放入到数字栈中。
+ *
  * @author by Eleven on 2021/12/29
  */
 public class BracketStack {
-    public static boolean isOk(String s) {        //s表示的就是待匹配的括号串 [}使用字符来表示 时间复杂度 O(n)
+    public static boolean isOk(String s) { // s表示的就是待匹配的括号串 [}使用字符来表示 时间复杂度 O(n)
         MyStack<Character> brackets = new ArrayStack<Character>(20);
         char c[] = s.toCharArray();
         Character top;
