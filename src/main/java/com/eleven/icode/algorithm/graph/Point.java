5eb5cdfb959fa73416c80e0843dda3dabf6469e3
Y31201308 <906271196@qq.com>
2022-01-02 00:04:29 +0800

    feat: add test

---
+++ src/main/java/com/eleven/icode/algorithm/graph/Point.java
@@ -0,0 +1,9 @@
+package com.eleven.icode.algorithm.graph;
+
+/**
+ * @author by YingLong on 2022/1/1
+ */
+public class Point {
+    int x;
+    int y;
+}
---
+++ src/main/java/com/eleven/icode/algorithm/hash/Hash.java
@@ -0,0 +1,44 @@
+package com.eleven.icode.algorithm.hash;
+
+/**
+ * @author by YingLong on 2022/1/1
+ */
+public class Hash {
+    int size;
+
+    public int hash_1(String key) {
+        int hash = 0;
+        int i;
+        for (i = 0; i < key.length(); ++i) {
+            hash = 33 * hash + key.charAt(i);
+        }
+        return Math.abs(hash) % size;
+    }
+
+    public int hash_2(String key) {
+        final int p = 16777619;
+        int hash = (int) 2166136261L;
+        for (int i = 0; i < key.length(); i++) {
+            hash = (hash ^ key.charAt(i)) * p;
+        }
+        hash += hash << 13;
+        hash ^= hash >> 7;
+        hash += hash << 3;
+        hash ^= hash >> 17;
+        hash += hash << 5;
+        return Math.abs(hash) % size;
+    }
+
+    public int hash_3(String key) {
+        int hash, i;
+        for (hash = 0, i = 0; i < key.length(); ++i) {
+            hash += key.charAt(i);
+            hash += hash << 10;
+            hash ^= hash >> 6;
+        }
+        hash += hash << 3;
+        hash ^= hash >> 11;
+        hash += hash << 15;
+        return Math.abs(hash) % size;
+    }
+}
